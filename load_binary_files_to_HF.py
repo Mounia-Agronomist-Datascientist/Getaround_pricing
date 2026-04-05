3fbd0266201e62c64ad25a36580ef5d82849379f
MouniaTonazzini <mounia.tonazzini@gmail.com>
2026-04-05 22:38:52 +0200

    Added preprocessing for loading to HF

--- load_binary_files_to_HF.py
+++ load_binary_files_to_HF.py
@@ -7,10 +7,19 @@ from dotenv import load_dotenv
 # Load variables from .env
 load_dotenv()
 
+# Upload the model
 api = HfApi(token=os.environ.get("HF_TOKEN"))
 api.upload_file(
     path_or_fileobj="MODELS/rental_price_predictor.joblib",
     path_in_repo="MODELS/rental_price_predictor.joblib",
+    repo_id="MouniaT/FastAPI_GetAround",
+    repo_type="space"
+)
+
+# Upload the processor
+api.upload_file(
+    path_or_fileobj="API/MODELS/preprocessing_pipeline.pkl",
+    path_in_repo="MODELS/preprocessing_pipeline.pkl",
     repo_id="MouniaT/FastAPI_GetAround",
     repo_type="space"
 )
